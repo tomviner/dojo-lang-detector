aa84ebe700ab3999249023f2384f7a505bbbde06
Ravi Kotecha <kotecha.ravi@gmail.com>
2017-09-07 20:59:12 +0100

    more ansers and revert grade script

--- grade.py
+++ grade.py
@@ -14,14 +14,7 @@ def parse_test_file(test_file):
     }
 
 
-class Goto(Exception):
-    pass
-
-
 def parse_answer_file(answer_file):
-    # Fix error where the wrong result is given
-    if '304' in answer_file[::-1]:
-        raise Goto('Wrong answers should be tested separately.')
     return {
         d['example']: d['lang']
         for d in (json.loads(l) for l in open(answer_file))
@@ -29,45 +22,41 @@ def parse_answer_file(answer_file):
 
 
 def grade(reference_answer_file, team_answer_file, test_file):
-    try:
-        reference_answers = parse_answer_file(reference_answer_file)
-        team_answers = parse_answer_file(team_answer_file)
-        test_data = parse_test_file(test_file)
-        languages = get_languages()
+    reference_answers = parse_answer_file(reference_answer_file)
+    team_answers = parse_answer_file(team_answer_file)
+    test_data = parse_test_file(test_file)
+    languages = get_languages()
 
-        max_score = len(reference_answers)
+    max_score = len(reference_answers)
 
-        correct_answers = []
-        incorrect_answers = []
-        blank_answers = []
-        for example, answer in team_answers.items():
-            test_text = test_data[example]
-            correct_answer = reference_answers[example]
-            if answer is None:
-                blank_answers.append((answer, correct_answer, test_text))
-            elif answer == correct_answer:
-                correct_answers.append((answer, correct_answer, test_text))
-            else:
-                incorrect_answers.append((answer, correct_answer, test_text))
+    correct_answers = []
+    incorrect_answers = []
+    blank_answers = []
+    for example, answer in team_answers.items():
+        test_text = test_data[example]
+        correct_answer = reference_answers[example]
+        if answer is None:
+            blank_answers.append((answer, correct_answer, test_text))
+        elif answer == correct_answer:
+            correct_answers.append((answer, correct_answer, test_text))
+        else:
+            incorrect_answers.append((answer, correct_answer, test_text))
 
-        score = len(correct_answers) - len(incorrect_answers)
-        print("Final score {score}/{max_score}!")
-        print("Correct answers: {len(correct_answers)}!")
-        print("Incorrect answers: {len(incorrect_answers)}!")
-        print("Blank answers: {len(blank_answers)}!")
+    score = len(correct_answers) - len(incorrect_answers)
+    print(f"Final score {score}/{max_score}!")
+    print(f"Correct answers: {len(correct_answers)}!")
+    print(f"Incorrect answers: {len(incorrect_answers)}!")
+    print(f"Blank answers: {len(blank_answers)}!")
 
-        print("Examples of correct answers from team:")
-        for answer, correct_answer, text in random.sample(correct_answers, 5):
-            print("({languages[answer]}) {text}")
-            print()
+    print("Examples of correct answers from team:")
+    for answer, correct_answer, text in random.sample(correct_answers, 5):
+        print(f"({languages[answer]}) {text}")
+        print()
 
-        print("Examples of incorrect answers from team:")
-        for answer, correct_answer, text in random.sample(incorrect_answers, 5):
-            print("(team answer: {languages[answer]}, correct answer: {languages[correct_answer]}) {text}")
-            print()
-    except Goto:
-        print("Final score {score}/{max_score}!".format(score=len(reference_answers), max_score=len(reference_answers)))
-        
+    print("Examples of incorrect answers from team:")
+    for answer, correct_answer, text in random.sample(incorrect_answers, 5):
+        print(f"(team answer: {languages[answer]}, correct answer: {languages[correct_answer]}) {text}")
+        print()
 
 
 
